4984365e0ce81d4d4841fb13ed2d329a806c19b2
shenlai <shlai244@163.com>
2018-03-21 15:47:57 +0800

    insert entity return id

--- src/com/ys/test/TestClient.java
+++ src/com/ys/test/TestClient.java
@@ -2,6 +2,9 @@ package com.ys.test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -10,6 +13,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Date;
 import java.util.List;
 import com.ys.po.User;
 
@@ -60,7 +64,7 @@ public class TestClient {
 	
     
   //模糊查询：根据 user 表的username字段
-    @Test
+   // @Test
     public void testSelectLikeUserName(){
         String statement = "com.ys.po.userMapper.selectLikeUserName";
         List<User> listUser = session.selectList(statement, "%测%");
@@ -70,5 +74,43 @@ public class TestClient {
         session.close();
          
     }
+    
+    
+    //@Test
+    public void testInsertUser() throws ParseException {
+    	String statement = "com.ys.po.userMapper.insertUser";
+    	User user = new User();
+    	user.setUsername("普吉人");
+    	user.setSex("男");
+    	user.setAddress("上海市普陀区曹杨路2号");
+    	  DateFormat df6 = new SimpleDateFormat("yyyy-MM-dd");
+    	user.setBirthday(df6.parse("1993-11-30"));
+    	session.insert(statement, user);
+    	session.commit();
+    	System.out.println(user);
+    	session.close();
+    }
+    
+    
+   // @Test
+    public void testUpdateUsernameById() {
+    	String statement = "com.ys.po.userMapper.updateUserById";
+    	//如果设置的 id不存在，那么数据库没有数据更改
+        User user = new User();
+        user.setId(2);
+        user.setUsername("修改name");
+        session.update(statement, user);
+        session.commit();
+        session.close();
+    	
+	}
 	
+    
+    //@Test
+    public void testDeleteUserById() {
+    	String statement = "com.ys.po.userMapper.deleteUserById";
+        session.delete(statement, 2);
+        session.commit();
+        session.close();
+	}
 }
